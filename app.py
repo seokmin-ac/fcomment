395de4843f47a64a70406639696cedb6c1c66642
Seokmin Hong <kyma123@unist.ac.kr>
2020-09-04 10:31:18 +0900

    Implement endpoints for get/post articles

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import os
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, abort
 from flask_cors import CORS
-from models import setup_db
+from models import setup_db, Article, Comment, db_rollback
+from auth import AuthError
 
 try:
     from dotenv import load_dotenv
@@ -9,91 +10,109 @@ try:
 except Exception:
     pass
 
-def create_app():
-    app = Flask(__name__)
-    CORS(app)
-
-
-    @app.route('/articles')
-    def get_articles():
-        pass
-
-
-    @app.route('/articles', methods=['POST'])
-    def post_articles():
-        pass
-
-
-    @app.route('/articles', methods=['DELETE'])
-    def delete_articles():
-        pass
-
-
-    @app.route('/articles/<int:id>')
-    def get_comments_from_article():
-        pass
-
-
-    @app.route('/articles/<int:id>', methods=['POST'])
-    def post_comment_to_article():
-        pass
-
-
-    @app.route('/comments')
-    def get_comments():
-        pass
-
-
-    @app.route('/comments/<int:id>')
-    def get_comment():
-        pass
-
-
-    @app.route('/comments/<int:id>', methods=['POST'])
-    def post_reply():
-        pass
-
-
-    @app.route('/comments/<int:id>', methods=['PATCH'])
-    def edit_comment():
-        pass
-
-
-    @app.route('/comments/<int:id>', methods=['DELETE'])
-    def delete_comment():
-        pass
-
-    
-    @app.errorhandler(404)
-    def not_found(error):
-        return jsonify({
-          'success': False,
-          'error': 404,
-          'message': 'Resource not found.'
-        }), 404
-
-
-    @app.errorhandler(422)
-    def unprocessable_entity(error):
-        return jsonify({
-          'success': False,
-          'error': 422,
-          'message': 'Unprocessable entity.'
-        }), 422
-
-
-    @app.errorhandler(500)
-    def internal_server_error(error):
-        return jsonify({
-          'success': False,
-          'error': 500,
-          'message': 'Internal Server error.'
-        }), 500
-
-    return app
-
-app = create_app()
+app = Flask(__name__)
+CORS(app)
 setup_db(app, os.environ['DATABASE_URL'])
+
+
+@app.route('/articles')
+def get_articles():
+    articles = Article.query.all()
+    return jsonify({
+        'success': True,
+        'articles': [a.format() for a in articles]
+    })
+
+
+@app.route('/articles', methods=['POST'])
+def post_articles():
+    try:
+        article = Article(
+            id=request.json['id']
+        )
+        article.insert()
+        return jsonify({
+            'success': True,
+            'id': article.id
+        })
+    except Exception:
+        db_rollback()
+        abort(422)
+
+
+@app.route('/articles', methods=['DELETE'])
+def delete_articles():
+    pass
+
+
+@app.route('/articles/<string:id>')
+def get_comments_from_article(id):
+    pass
+
+
+@app.route('/articles/<string:id>', methods=['POST'])
+def post_comment_to_article(id):
+    pass
+
+
+@app.route('/comments')
+def get_comments():
+    pass
+
+
+@app.route('/comments/<int:id>')
+def get_comment(id):
+    pass
+
+
+@app.route('/comments/<int:id>', methods=['POST'])
+def post_reply(id):
+    pass
+
+
+@app.route('/comments/<int:id>', methods=['PATCH'])
+def edit_comment(id):
+    pass
+
+
+@app.route('/comments/<int:id>', methods=['DELETE'])
+def delete_comment(id):
+    pass
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return jsonify({
+      'success': False,
+      'error': 404,
+      'message': 'Resource not found.'
+    }), 404
+
+
+@app.errorhandler(422)
+def unprocessable_entity(error):
+    return jsonify({
+      'success': False,
+      'error': 422,
+      'message': 'Unprocessable entity.'
+    }), 422
+
+
+@app.errorhandler(500)
+def internal_server_error(error):
+    return jsonify({
+      'success': False,
+      'error': 500,
+      'message': 'Internal Server error.'
+    }), 500
+
+@app.error_handler(AuthError)
+def auth_error(error):
+    return jsonify({
+        'success': False,
+        'error': error.status_code,
+        'message': f'{error.error.code}: {error.error.description}'
+    }), error.status_code
 
 
 if __name__ == '__main__':
--- models.py
+++ models.py
@@ -1,6 +1,6 @@
+import sys
 from sqlalchemy import Column, String, Integer, ForeignKey
 from flask_sqlalchemy import SQLAlchemy
-import uuid
 
 db = SQLAlchemy()
 
@@ -11,15 +11,38 @@ def setup_db(app, database_path):
     db.init_app(app)
     db.create_all()
 
+
+def db_rollback():
+    db.session.rollback()
+    print(sys.exc_info())
+    db.session.close()
+
+
 class Article(db.Model):
     __tablename__ = 'articles'
 
     id = Column(String, primary_key=True)
 
+    def format(self):
+        return {
+            'id': self.id
+        }
+
+
 class Comment(db.Model):
     __tablename__ = 'comments'
 
     id = Column(Integer, primary_key=True)
-    text = Column(String)
+    user = Column(String)
+    content = Column(String)
     article = Column(String, ForeignKey('articles.id'))
     parent = Column(Integer, ForeignKey('comments.id'))
+
+    def format(self):
+        return {
+            'id': self.id,
+            'user': self.user,
+            'content': self.content,
+            'article': self.article,
+            'parent': self.parent
+        }
